5cc4a7b8546204c2abfd7b33fb9ec66edce4bf6e
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2019-02-26 12:27:17 -0500

    Create Setup.js

---
+++ 3.0_Broken/Setup.js
@@ -0,0 +1,21 @@
+var seedVal;
+var biomeSel;
+var biomeSize = 30000;
+var noiseCoeff = 0.0131;
+var blocksPlaced = 0;
+var totalBlocksPlaced = 0;
+
+function setup() {
+  createCanvas(windowWidth*15, windowHeight);
+	background(51, 204, 255);
+	var seedVal = 34875387875;
+};
+
+function keyPressed() {
+	var seedVal;
+	var biomeSel;
+	var biomeSize = 30000;
+	var noiseCoeff = 0.0131;
+	var blocksPlaced = 0;
+	var totalBlocksPlaced = 0;
+}
